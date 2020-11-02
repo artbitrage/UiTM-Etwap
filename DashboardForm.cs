11be7c77b97d1aaa5a05fc38427f3d7341d72749
Syafiq Hadzir <syafiqhadzir@live.com.my>
2020-11-02 17:16:36 +0800

    Code cleaning

--- DashboardForm.cs
+++ DashboardForm.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Etwaps_Detector
